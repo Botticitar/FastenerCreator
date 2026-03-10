7029c91fb6a6e89c343645675e3ae09ec66d1676
Botticitar <maricza.peter@gmail.com>
2026-03-11 00:47:05 +0100

    Started with the framework.

---
+++ Program/src/stepgen_app/main.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+
+from src.stepgen_app.validator import #BaseModel instances for washer and bolt come here
+from src.stepgen_app.step_generator import #model generator class comes here 
+
+app=FastAPI()
+
---
+++ Program/src/stepgen_app/validator.py
@@ -0,0 +1,4 @@
+from pydantic import BaseModel, Field, model_validator
+from src.stepgen_app.data_checks import #name of data checks class
+
+class BoltRequest(BaseModel)
